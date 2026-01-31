efa6cf6a376ff5dbba91738fbeee1f95a0b53951
VeidaB <vbuhrz@hawk.illinoistech.edu>
2026-01-30 20:53:09 -0500

    Procinfo done

--- src/procinfo.c
+++ src/procinfo.c
@@ -24,6 +24,9 @@ int main(int c,char**v){
  char state;
  char cmdline[1024];
  char path[50];
+ char line[50];
+ long usertime;
+ long systime;
  FILE *f;
 
  //CHECK PID
@@ -38,20 +41,45 @@ int main(int c,char**v){
     }
  }
 
- printf("pid: %d\n", pid);
+ printf("\npid: %d\n", pid);
  
  //PRINT STATE
  snprintf(path, sizeof(path), "/proc/%d/stat", pid);
  f = fopen(path, "r");
  fscanf(f, "%*d %*[^)]%*c %c", &state);
- printf("State: %c\n", state);
  fclose(f);
+ printf("State: %c\n", state);
 
  //PRINT COMMAND LINE
  snprintf(path, sizeof(path), "/proc/%d/cmdline", pid);
  f = fopen(path, "r");
  if (fgets(cmdline, sizeof(cmdline), f)){
     printf("Command line: %s\n", cmdline);
+ }
+ fclose(f);
+
+ //PRINT CPU TIME
+ snprintf(path, sizeof(path), "/proc/%d/stat", pid);
+ f = fopen(path, "r");
+ fscanf(f, "%*d %*[^)]%*c "
+        "%*c %*d %*d %*d %*d %*d "
+        "%*u %*u %*u %*u "
+        "%ld %ld", &usertime, &systime);
+ fclose(f);
+
+ 
+ long ticks = sysconf(_SC_CLK_TCK);
+ printf("User CPU time: %.2f\n", (double)usertime/ ticks);
+ printf("System CPU time: %.2f\n", (double)systime/ ticks);
+
+ //PRINT MEMORY USAGE
+ snprintf(path, sizeof(path), "/proc/%d/status", pid);
+ f = fopen(path, "r");
+ while(fgets(line, sizeof(line), f)){
+    if(strncmp(line, "VmRSS:", 6) == 0){
+        printf("%s\n", line);
+        break;
+    }
  }
  fclose(f);
 
