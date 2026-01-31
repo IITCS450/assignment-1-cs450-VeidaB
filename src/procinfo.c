45dda708245ba9b5610ceccd450541c8a0dce736
VeidaB <vbuhrz@hawk.illinoistech.edu>
2026-01-30 20:26:52 -0500

    Print pid, state, cmdline, check pid

--- src/procinfo.c
+++ src/procinfo.c
@@ -2,10 +2,58 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <unistd.h>
-static void usage(const char *a){fprintf(stderr,"Usage: %s <pid>\n",a); exit(1);}
-static int isnum(const char*s){for(;*s;s++) if(!isdigit(*s)) return 0; return 1;}
+#include <errno.h>
+#include <signal.h>
+
+static void usage(const char *a){
+    fprintf(stderr,"Usage: %s <pid>\n",a); 
+    exit(1);
+}
+
+static int isnum(const char*s){
+    for(;*s;s++) 
+      if(!isdigit(*s)) 
+      return 0; 
+    return 1;
+}
+
 int main(int c,char**v){
  if(c!=2||!isnum(v[1])) usage(v[0]);
- printf("TODO: implement procinfo\n");
+ 
+ int pid = atoi(v[1]);
+ char state;
+ char cmdline[1024];
+ char path[50];
+ FILE *f;
+
+ //CHECK PID
+ int check = kill(pid, 0);
+ if (check == -1){
+    if(errno == ESRCH){
+      printf("PID not found.\n");
+      return 1;
+    } else if(errno == EPERM){
+      printf("Permission denied.\n");
+      return 1;
+    }
+ }
+
+ printf("pid: %d\n", pid);
+ 
+ //PRINT STATE
+ snprintf(path, sizeof(path), "/proc/%d/stat", pid);
+ f = fopen(path, "r");
+ fscanf(f, "%*d %*[^)]%*c %c", &state);
+ printf("State: %c\n", state);
+ fclose(f);
+
+ //PRINT COMMAND LINE
+ snprintf(path, sizeof(path), "/proc/%d/cmdline", pid);
+ f = fopen(path, "r");
+ if (fgets(cmdline, sizeof(cmdline), f)){
+    printf("Command line: %s\n", cmdline);
+ }
+ fclose(f);
+
  return 0;
 }
